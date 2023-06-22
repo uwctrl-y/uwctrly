c33db1353146970cecd4c89844354058e7728c6f
Da Yun Chung <77592683+dchung29@users.noreply.github.com>
2023-06-21 20:53:15 -0700

    splash screen basic
    telling myself its fine for now cuz its gonna change lmfao (it looks kinda funny)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <home/>
-  <about/>
+  <splash-screen :isLoading="isLoading"/>
+  <main v-if="!isLoading">
+    <home/>
+    <about/>
+  </main>
 </template>
 
 <script>
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import SplashScreen from "@/components/splashScreen.vue";
+
 export default {
   name: 'App',
-  components: {About, Home}
+  components: {SplashScreen, About, Home},
+  data() {
+    return { isLoading: true };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1800);
+  }
 };
 </script>
-
 <style>
 </style>
---
+++ src/components/splashScreen.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="splash-screen">
+    <div :class="{ loader: true, fadeout: !isLoading, keys}">
+      <img src="@/assets/keyanimated.gif" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: "splashScreen",
+  props: ["isLoading"],
+}
+</script>
+
+<style>
+.splash-screen{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+  z-index: 1;
+  overflow: hidden;
+}
+
+.keys{
+  width: 38%;
+}
+
+.fadeout {
+  animation: fadeout 0.2s forwards;
+}
+
+@keyframes fadeout {
+  to {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+
+</style>
