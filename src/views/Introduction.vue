2cee17c7817e48b49036d4b72accc12b1c282aa0
aashi-s <shah.aashi28@gmail.com>
2023-07-20 13:25:12 -0400

    made design responsive and animated graphic

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,21 +1,24 @@
 <template>
-  <section>
+  <section style="background-color: #09702C;">
     <div class="introduction-section">
       <div class="intro-left">
         <p class="section-number">01.</p>
-        <p class="section-title">introducing CTRL + y</p>
+        <p class="section-title">About CTRL + y</p>
       </div>
-      <div class="intro-right">        
+      <div class="intro-right">
         <SlidingTextIntroduction :heading="heading" :paragraph="paragraph" v-if="reachedIntro" />
         <p class="section-heading" v-else>
-          We aim to foster <i>innovative design solutions</i> for emerging technologies.
+          Imagine the future at CTRL + y
         </p>
         <p class="section-content" v-if="!reachedIntro">{{ paragraph }}</p>
       </div>
     </div>
-    <div class="paintbrush-graphic">
+    <div class="paintbrush-graphic" ref="background">
+
       <img class="svg-paintbrush" src="../assets/paintbrushGraphic.svg" />
+
     </div>
+
   </section>
 </template>
 
@@ -23,12 +26,31 @@
 import SlidingTextIntroduction from './SlidingTextIntroduction.vue';
 
 export default {
+  mounted() {
+    document.addEventListener('scroll', this.handleScroll)
+  },
+  unmounted() {
+    document.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll(evt) {
+      const scrollY = window.scrollY - 1900
+
+      const maxBackgroundSize = 120
+      const backgroundSize = scrollY / (maxBackgroundSize - 100) // increases as user scrolls
+
+      // zoom the background at a slower rate
+      this.$refs.background.style.transform =
+        'scale(' + (100 + backgroundSize * 0.1) / 100 + ')'
+    }
+
+  },
   components: { SlidingTextIntroduction },
   props: ['reachedIntro'],
   data() {
-    const headingString = "We aim to foster innovative design solutions for emerging technologies."
-    const headingArray = headingString.split(" ").map(string => ({ content: string, italic: string === "innovative" || string === "design" || string === "solutions" }))
-    const paragraph = "The rising popularity of emerging technologies (such as artificial intelligence and artificial reality) pose a new type of design challenge to UI/UX designers. Aspiring UI/UX designers, in particular, may find this challenge difficult due to limited experience in these fields and scarce resources to reference. A group of Waterloo students came together to present a solution to this issue. The designathon was dubbed CTRL + y in honour of the popular keyboard shortcut for ‘redo.’ It symbolizes how we’re taking the resources of the past to design the future."
+    const headingString = "Imagine the future at CTRL + y"
+    const headingArray = headingString.split(" ")
+    const paragraph = ["The rising popularity of emerging technologies (such as artificial intelligence and artificial reality) pose a new type of design challenge to UI/UX designers. Aspiring UI/UX designers, in particular, may find this challenge difficult due to limited experience in these fields and scarce resources to reference.", "A group of students came together to present a solution to this issue. The designathon was dubbed CTRL + y in honour of the popular keyboard shortcut for ‘redo.’ It symbolizes how we’re taking the resources of the past to design the future."]
     return {
       heading: headingArray,
       paragraph: paragraph,
@@ -39,21 +61,18 @@ export default {
 
 <style scoped>
 .introduction-section {
-  background-color: #09702C;
   color: #E4E4D0;
-  font-family: Roboto;
-  font-style: normal;
   display: flex;
   justify-content: space-between;
-  padding: 103px 83px 235px 83px;
+  padding: 103px 83px 197px 83px;
 }
 
 .intro-left {
-  width: 15%;
+  width: 210px;
 }
 
 .intro-right {
-  width: 60%;
+  width: 55%;
 }
 
 .section-number {
@@ -89,14 +108,69 @@ export default {
 }
 
 .paintbrush-graphic {
-  background-color: #09702C;
   width: 100%;
   height: fit-content;
   overflow: hidden;
-  padding: 32px;
+  padding: 40px;
+  position: relative;
 }
 
 .svg-paintbrush {
   width: 100%;
 }
+
+@media screen and (max-width: 900px) {
+  .introduction-section {
+    flex-direction: column;
+    padding-bottom: 80px;
+  }  
+  .intro-left {
+    width: 100%;
+    margin-bottom: 40px;
+  }
+
+  .intro-right {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .introduction-section {
+    padding: 60px 40px 80px 40px;
+  }
+
+  .paintbrush-graphic {
+    padding: 20px;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .introduction-section {
+    padding-bottom: 20px;
+  }
+  .section-number {
+    font-size: 15px;
+    letter-spacing: -.5px;
+  }
+
+  .section-title {
+    font-size: 25px;
+    letter-spacing: -1px;
+  }
+} 
+
+@media screen and (max-width: 350px) {
+  .introduction-section {
+    padding-top: 40px;
+  }
+
+  .intro-left {
+    margin-bottom: 20px;
+  }
+  .section-title {
+    font-size: 18px;
+  }
+
+}
+
 </style>
