1cb92d54870370e1f14f22ba8ca2bf5a0ccd484a
aashi-s <shah.aashi28@gmail.com>
2023-07-20 17:24:59 -0400

    fixed background colour for graphic

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,5 +1,5 @@
 <template>
-  <section style="background-color: #09702C;">
+  <section>
     <div class="introduction-section">
       <div class="intro-left">
         <p class="section-number">01.</p>
@@ -13,10 +13,10 @@
         <p class="section-content" v-if="!reachedIntro">{{ paragraph }}</p>
       </div>
     </div>
-    <div class="paintbrush-graphic" ref="background">
-
-      <img class="svg-paintbrush" src="../assets/paintbrushGraphic.svg" />
-
+    <div class="paintbrush-graphic">
+      <div ref="background">
+        <img class="svg-paintbrush" src="../assets/paintbrushGraphic.svg" />
+      </div>
     </div>
 
   </section>
@@ -65,6 +65,7 @@ export default {
   display: flex;
   justify-content: space-between;
   padding: 103px 83px 197px 83px;
+  background-color: #09702C;
 }
 
 .intro-left {
@@ -72,7 +73,7 @@ export default {
 }
 
 .intro-right {
-  width: 55%;
+  width: 70%;
 }
 
 .section-number {
@@ -111,19 +112,22 @@ export default {
   width: 100%;
   height: fit-content;
   overflow: hidden;
-  padding: 40px;
   position: relative;
+  text-align: center;
 }
 
 .svg-paintbrush {
   width: 100%;
+  padding: 40px;
+  max-width: 99vw;
 }
 
 @media screen and (max-width: 900px) {
   .introduction-section {
     flex-direction: column;
     padding-bottom: 80px;
-  }  
+  }
+
   .intro-left {
     width: 100%;
     margin-bottom: 40px;
@@ -148,6 +152,7 @@ export default {
   .introduction-section {
     padding-bottom: 20px;
   }
+
   .section-number {
     font-size: 15px;
     letter-spacing: -.5px;
@@ -157,7 +162,7 @@ export default {
     font-size: 25px;
     letter-spacing: -1px;
   }
-} 
+}
 
 @media screen and (max-width: 350px) {
   .introduction-section {
@@ -167,10 +172,10 @@ export default {
   .intro-left {
     margin-bottom: 20px;
   }
+
   .section-title {
     font-size: 18px;
   }
 
 }
-
 </style>
