e4acadc74a64943569877091a504a5cb9700b250
Da Yun Chung <77592683+dchung29@users.noreply.github.com>
2023-08-14 09:32:56 -0700

    Ourteam Fixes

--- src/views/OurTeam.vue
+++ src/views/OurTeam.vue
@@ -1,166 +1,172 @@
 <template>
-    <section class="our-team-section" ref="ourTeamSection">
-        <div class="parallax-container">
-            <div class="heading-container">
-                <h1 class="heading-text">Our Team</h1>
-            </div>
-            <div class="sponsor-box-two">
-                <img
-                        class="circle"
-                        :style="{ top: `${scrollOffset * 0.5}px` }"
-                        :src="currentImage"
-                        ref="graphic"
-                />
-            </div>
-        </div>
-    </section>
+  <section class="our-team-section" ref="ourTeamSection">
+    <div class="parallax-container">
+      <div class="heading-container">
+        <h1 class="heading-text">Our Team</h1>
+      </div>
+      <div class="sponsor-box-two">
+        <img
+            class="circle"
+            :style="{ top: `${scrollOffset * 0.5}px` }"
+            :src="currentImage"
+            ref="graphic"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            scrollOffset: 0,
-            prevScrollY: 0,
-            imageUrls: [
-                "src/assets/ourTeam/testImages/zee.png",
-                "src/assets/ourTeam/testImages/image1.png",
-                "src/assets/ourTeam/testImages/image2.png",
-                "src/assets/ourTeam/testImages/image3.png",
-                "src/assets/ourTeam/testImages/image4.png",
-                "src/assets/ourTeam/testImages/image5.png",
-                "src/assets/ourTeam/testImages/image6.png",
-                "src/assets/ourTeam/testImages/image7.png",
-                "src/assets/ourTeam/testImages/image8.png",
-                "src/assets/ourTeam/testImages/image9.png",
-                "src/assets/ourTeam/testImages/image10.png",
-                "src/assets/ourTeam/testImages/image11.png",
-            ],
-            currentImageIndex: 0,
-            lockScreen: true,
-        };
+  data() {
+    return {
+      scrollOffset: 0,
+      prevScrollY: 0,
+      imageUrls: [
+        "src/assets/ourTeam/testImages/zee.png",
+        "src/assets/ourTeam/testImages/image1.png",
+        "src/assets/ourTeam/testImages/image2.png",
+        "src/assets/ourTeam/testImages/image3.png",
+        "src/assets/ourTeam/testImages/image4.png",
+        "src/assets/ourTeam/testImages/image5.png",
+        "src/assets/ourTeam/testImages/image6.png",
+        "src/assets/ourTeam/testImages/image7.png",
+        "src/assets/ourTeam/testImages/image8.png",
+        "src/assets/ourTeam/testImages/image9.png",
+        "src/assets/ourTeam/testImages/image10.png",
+        "src/assets/ourTeam/testImages/image11.png",
+      ],
+      currentImageIndex: 0,
+      lockScreen: true,
+    };
+  },
+  computed: {
+    currentImage() {
+      return this.imageUrls[this.currentImageIndex];
     },
-    computed: {
-        currentImage() {
-            return this.imageUrls[this.currentImageIndex];
-        },
-    },
-    mounted() {
-        window.addEventListener("scroll", this.onScroll);
-         const options = {
-             root: null,
-             rootMargin: "0px",
-             threshold: 1.0,
-        };
-        const observer = new IntersectionObserver(
-            this.handleSectionIntersection,
-            options
-        );
-        observer.observe(this.$refs.ourTeamSection);
-    },
-    beforeDestroy() {
-        window.removeEventListener("scroll", this.onScroll);
-    },
-    methods: {
-      onScroll() {
-        const currentScrollY = window.scrollY;
-        const scrollDirection = currentScrollY > this.prevScrollY ? "down" : "up";
-        const scrollDifference = Math.abs(currentScrollY - this.prevScrollY);
-        const increment = 0.5;
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+    const observer = new IntersectionObserver(
+        this.handleSectionIntersection,
+        options
+    );
+    observer.observe(this.$refs.ourTeamSection);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      if (this.lockScreen) {
+        return;
+      }
+      const currentScrollY = window.scrollY;
+      const scrollDirection = currentScrollY > this.prevScrollY ? "down" : "up";
+      const scrollDifference = Math.abs(currentScrollY - this.prevScrollY);
+      const increment = 0.5;
 
+      if (scrollDirection === "down") {
+        this.scrollOffset += increment * scrollDifference;
+      } else {
+        this.scrollOffset -= increment * scrollDifference;
+      }
+
+      if (scrollDifference > 15) {
         if (scrollDirection === "down") {
-          this.scrollOffset += increment * scrollDifference;
+          this.currentImageIndex = Math.min(
+              this.currentImageIndex + 1,
+              this.imageUrls.length - 1
+          );
         } else {
-          this.scrollOffset -= increment * scrollDifference;
+          this.currentImageIndex = Math.max(this.currentImageIndex - 1, 0);
         }
-
-        if (this.lockScreen && scrollDifference > 15) {
-          if (scrollDirection === "down") {
-            this.currentImageIndex = Math.min(
-                this.currentImageIndex + 1,
-                this.imageUrls.length - 1
-            );
-          } else {
-            this.currentImageIndex = Math.max(this.currentImageIndex - 1, 0);
-          }
-          this.prevScrollY = currentScrollY;
-        }
-      },
-        handleSectionIntersection(entries) {
-            const inView = entries[0].isIntersecting;
-
-            if (inView) {
-                this.lockScreen = false;
-            } else {
-                this.lockScreen = true;
-            }
-        },
+        this.prevScrollY = currentScrollY;
+      }
     },
+    handleSectionIntersection(entries) {
+      console.log("Intersection changed:", entries);
+      const inView = entries[0].isIntersecting;
+
+      if (inView) {
+        this.lockScreen = false;
+        this.scrollOffset = 0;
+        this.currentImageIndex = 0;
+      } else {
+        this.lockScreen = true;
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
 .our-team-section {
-    border-radius: 16px;
-    padding: 120px;
-    color: #09702C;
-    position: relative;
-    min-height: 80vh;
+  border-radius: 16px;
+  padding: 120px;
+  color: #09702C;
+  position: relative;
+  min-height: 80vh;
 }
 
-.heading-container{
-    position: absolute;
-    z-index: 1;
+.heading-container {
+  position: absolute;
+  z-index: 1;
 }
 
 .heading-text {
-    font-size: 180px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 180px;
-    letter-spacing: -9px;
-    text-transform: uppercase;
-    margin-bottom: 60vh;
+  font-size: 180px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 180px;
+  letter-spacing: -9px;
+  text-transform: uppercase;
+  margin-bottom: 60vh;
 }
 
 .parallax-container {
-    overflow: hidden;
-    position: relative;
-    height: 80vh;
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  overflow: hidden;
+  position: relative;
+  height: 80vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .sponsor-box-two {
-    align-items: center;
-    text-align: center;
-    width: 100%;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .circle {
-    border-radius: 50%;
-    max-width: 100%;
-    transition: top 0.2s ease; /* Add a smooth transition for the parallax effect */
+  border-radius: 50%;
+  max-width: 100%;
+  transition: top 0.2s ease;
 }
 
 @media screen and (max-width: 768px) {
-    .heading-text {
-        font-size: 12vw;
-        letter-spacing: -0.1em;
-    }
+  .heading-text {
+    font-size: 12vw;
+    letter-spacing: -0.1em;
+  }
 }
 
 @media screen and (max-width: 480px) {
-    .heading-text {
-        font-size: 16vw;
-        letter-spacing: -0.05em;
-    }
+  .heading-text {
+    font-size: 16vw;
+    letter-spacing: -0.05em;
+  }
 }
 
 </style>
